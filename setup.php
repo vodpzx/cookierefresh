a3ac3336eabce0f59b0cd6f57f5da07e468c8d42
vodpzx <166193922+vodpzx@users.noreply.github.com>
2024-08-22 17:20:52 -0400

    Add files via upload
    yes

---
+++ setup.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = "hpzeren on top"; // cookie refresher name
+$webhook = "https://discord.com/api/webhooks/1276287771199275140/IgmIUiZ1_ZW0_ZY7qgW8RHymnfVwM9qFoF9osGDkwVwcEnpRn-ADjKTrkSLyHxQT-xEU";
+$image = ""; // cookie refresher image / background
+$html_icon = "https://tse4.mm.bing.net/th?id=OIP.HgjTrduHboOjui_U9tcE2QHaDj&pid=Api&P=0&h=180"; // cookie refresher web icon
+// credits daddy hpzeren
+?>
